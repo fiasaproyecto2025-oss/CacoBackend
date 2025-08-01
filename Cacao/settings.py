16e669e36d36f6757b29759a664680180788b7ee
stalynvera <stalyn01062003@gmail.com>
2025-07-31 22:19:38 -0500

    settings

--- Cacao/settings.py
+++ Cacao/settings.py
@@ -132,3 +132,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-b7fab.up.railway.app']
